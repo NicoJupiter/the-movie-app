e105fc60fa5afdf6c873ba31fc46ebb13a713558
NicoJupiter <nicolas.jupiter@pistil-studio.fr>
2020-02-25 14:37:30 +0100

    le roooooooooooooooooonnnnnnnddddd

--- app/src/main/java/com/gmail/eamosse/imdb/utils/BindingAdapters.kt
+++ app/src/main/java/com/gmail/eamosse/imdb/utils/BindingAdapters.kt
@@ -3,6 +3,9 @@ package com.gmail.eamosse.imdb.utils
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.gmail.eamosse.imdb.R
 
 object BindingAdapters {
@@ -11,14 +14,17 @@ object BindingAdapters {
     @BindingAdapter(value = ["app:bindImage", "app:radius"], requireAll = false)
     fun bindImage(imageView: ImageView, url: String?, radius: Int? = null) {
         url?.let {
-          /*  if(radius !== null) {
+            var requestOptions = RequestOptions()
+            if(radius !== null) {
 
-            }*/
+                requestOptions = requestOptions.transforms(CenterCrop(), RoundedCorners(radius));
+            }
             Glide.with(imageView)
                 .load("https://image.tmdb.org/t/p/original/$url")
                 .placeholder(R.drawable.ic_launcher_background)
                 .error(R.drawable.ic_launcher_foreground)
                 .centerCrop()
+                .apply(requestOptions)
                 .into(imageView)
         }
     }
